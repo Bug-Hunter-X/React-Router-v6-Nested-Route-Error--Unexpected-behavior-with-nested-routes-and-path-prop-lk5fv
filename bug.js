5653656d85ee0cc4062870b335ab54f5f69644a8
BugHunter-X <askzenai@gmail.com>
2025-02-04 01:04:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,29 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* This route will cause the error if the user navigates to /about/nested */}
+        <Route path="/about/nested" element={<Nested />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function About() {
+  return <h1>About</h1>;
+}
+
+function Nested() {
+  return <h1>Nested</h1>;
+}
+export default App;
+```
